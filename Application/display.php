358f4eb061c9d820c6f0172dd743a0ad0c1a96c3
Rochit Madamanchi <150697578+Rochit02@users.noreply.github.com>
2024-06-10 23:30:07 +0530

    Update display.php

--- Application/display.php
+++ Application/display.php
@@ -8,7 +8,7 @@ $conn = new mysqli("localhost", "root", "", "token_system");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Token Lists</title>
-    <link rel="stylesheet" href="styles3.css">
+    <link rel="stylesheet" href="css/styles3.css">
     <script>
         function fetchAndUpdate(url, elementId) {
             fetch(url)
