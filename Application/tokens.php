553b01a6b83cc80e772c253e8ec508a812bb7e63
Rochit Madamanchi <150697578+Rochit02@users.noreply.github.com>
2024-06-10 23:31:58 +0530

    Update tokens.php

--- Application/tokens.php
+++ Application/tokens.php
@@ -34,7 +34,7 @@ $not_arrived_tokens = $conn->query("SELECT token_number, name FROM tokens WHERE 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Token Generation</title>
-    <link rel="stylesheet" href="styles4.css">
+    <link rel="stylesheet" href="css/styles4.css">
     <script>
         function fetchAndUpdate(url, elementId) {
             fetch(url)
